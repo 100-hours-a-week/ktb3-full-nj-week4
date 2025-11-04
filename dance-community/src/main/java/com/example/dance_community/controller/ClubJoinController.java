5806a699e2abc07254ac01e1156af5136c6f6002
JINOJ37 <jinoj0423@gmail.com>
2025-11-05 00:26:34 +0900

    ✨ Feat : EventJoinController 구현
    이벤트 조인 컨트롤러 구현 완료

--- dance-community/src/main/java/com/example/dance_community/controller/ClubJoinController.java
+++ dance-community/src/main/java/com/example/dance_community/controller/ClubJoinController.java
@@ -23,35 +23,35 @@ public class ClubJoinController {
     private final ClubJoinService clubJoinService;
 
     @Operation(summary = "클럽 가입", description = "클럽에 가입합니다.")
-    @PostMapping("/club-joins")
+    @PostMapping()
     public ResponseEntity<ApiResponse<ClubJoinResponse>> createClubJoin(@GetUserId Long userId, @Valid @RequestBody ClubJoinCreateRequest clubJoinCreateRequest) {
         ClubJoinResponse clubJoinResponse = clubJoinService.createClubJoin(userId, clubJoinCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>("클럽 가입 성공", clubJoinResponse));
     }
 
     @Operation(summary = "사용자가 가입한 클럽 조회", description = "특정 사용자가 가입한 모든 클럽의 정보를 불러옵니다.")
-    @GetMapping("/club-joins/club")
+    @GetMapping("/club")
     public ResponseEntity<ApiResponse<List<ClubJoinResponse>>> getUserClubs(@GetUserId Long userId) {
         List<ClubJoinResponse> clubJoinResponses = clubJoinService.getUserClubs(userId);
         return ResponseEntity.ok(new ApiResponse<>("사용자가 가입한 클럽 조회 성공", clubJoinResponses));
     }
 
     @Operation(summary = "클럽에 가입한 사용자 조회", description = "특정 클럽에 가입한 모든 사용자의 정보를 불러옵니다.")
-    @GetMapping("/club-joins/user/{clubId}")
+    @GetMapping("/user/{clubId}")
     public ResponseEntity<ApiResponse<List<ClubJoinResponse>>> getClubUsers(@PathVariable Long clubId) {
         List<ClubJoinResponse> clubJoinResponses = clubJoinService.getClubUsers(clubId);
         return ResponseEntity.ok(new ApiResponse<>("클럽에 가입한 사용자 조회 성공", clubJoinResponses));
     }
 
     @Operation(summary = "클럽 가입 확인", description = "특정 사용자가 특정 클럽의 멤버인지 확인합니다.")
-    @GetMapping("/club-joins/check/{postId}")
+    @GetMapping("/check/{postId}")
     public ResponseEntity<Boolean> checkMembership(@GetUserId Long userId, @PathVariable Long postId) {
         boolean isMember = clubJoinService.isClubJoin(userId, postId);
         return ResponseEntity.ok(isMember);
     }
 
     @Operation(summary = "클럽 탈퇴", description = "클럽에서 탈퇴합니다.")
-    @DeleteMapping("/club-joins/{clubId}")
+    @DeleteMapping("/{clubId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> deleteClubJoin(@GetUserId Long userId, @PathVariable Long postId) {
         clubJoinService.deleteClubJoin(userId, postId);
